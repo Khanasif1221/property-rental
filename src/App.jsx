13ad40d284a8eac02c529a69cb9c12420482aa25
asifkhan12211 <asifkhan98007076446@gmail.com>
2025-03-24 11:29:25 +0545

    added new pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import './App.css'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       {/* <Navbar /> */}
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
+      <Testimonials/>
+      <Contact/>
     </div>
   )
 }
