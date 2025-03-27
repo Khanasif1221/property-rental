15a9ec40899dccc6ed81fbb0e4a6c1b1f3766740
asifkhan12211 <asifkhan98007076446@gmail.com>
2025-03-27 14:00:25 +0545

    edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
       className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
       <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About 
         Properties, Deticated to your Vision</p>
-        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
-            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg'/>
+        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20 '>
+            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg hover:scale-110  transition-transform duration-300'/>
             <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
                 <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
                 <div>
